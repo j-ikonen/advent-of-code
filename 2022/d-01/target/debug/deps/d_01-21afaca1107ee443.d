3d948883c358f6ad23965265e24b7ad9f13c97c4
Joonas Ikonen <joonaspikonen@gmail.com>
2023-07-08 15:20:46 +0300

    init 2022

---
+++ 2022/d-01/target/debug/deps/d_01-21afaca1107ee443.d
@@ -0,0 +1,5 @@
+/home/joonas/Code/advent-of-code/2022/d-01/target/debug/deps/d_01-21afaca1107ee443.rmeta: src/main.rs
+
+/home/joonas/Code/advent-of-code/2022/d-01/target/debug/deps/d_01-21afaca1107ee443.d: src/main.rs
+
+src/main.rs:
---
+++ 2022/d-01/target/debug/deps/d_01-ec5e11296469687f.d
@@ -0,0 +1,5 @@
+/home/joonas/Code/advent-of-code/2022/d-01/target/debug/deps/d_01-ec5e11296469687f.rmeta: src/main.rs
+
+/home/joonas/Code/advent-of-code/2022/d-01/target/debug/deps/d_01-ec5e11296469687f.d: src/main.rs
+
+src/main.rs:
